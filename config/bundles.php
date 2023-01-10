30078e2c2e8195f130b78329137c6c83ad85c25e
Magnus Nordlander <magnus@nordlander.se>
2023-01-10 11:14:23 +0100

    Added redeye/graphql-federation-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Redeye\GraphqlFederationBundle\RedeyeGraphqlFederationBundle::class => ['all' => true],
 ];
